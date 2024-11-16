1126503f9bcb4e46fd40dc44915f3c42513afd57
Samarth Narayan <kayaniv321@gmail.com>
2024-11-17 00:42:49 +0530

    added logos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <>
+				<title>Hack Arena</title>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+				<meta
+					name='description'
+					content='Web application that contains leetcode problems and video solutions'
+				/>
+			</>
+      <Component {...pageProps} />
+    </>
+  );
 }
