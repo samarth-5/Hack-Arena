53aae0fa5adeef517102266bca641ae310f2873a
Samarth Narayan <kayaniv321@gmail.com>
2024-11-17 01:48:41 +0530

    completed navbar functionality

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 type Props = {}
 
 export default function Navbar({}: Props) {
-  return (
-    <div>Navbar</div>
-  )
+
+    const handleClick=()=>{}
+
+    return (
+		<div className='flex items-center justify-between px-20'>
+			<Link href='/' className='flex items-center justify-center h-20'>
+				<Image src='/favicon.ico' alt='Hack Arena' height={50} width={50} />
+                <Image src='/logo.png' alt='Hack Arena' height={240} width={240} />
+			</Link>
+            <div className="flex gap-10 justify-between bg-black font-semibold text-white text-lg p-2 px-6 rounded-3xl">
+                <Link href='/problems' className="hover:text-xl transition-all">Problems</Link>
+                <Link href='/contests' className="hover:text-xl transition-all">Contests</Link>
+                <Link href='/about' className="hover:text-xl transition-all">About</Link>
+            </div>
+			<div className='flex items-center'>
+				<button className='bg-black font-semibold text-white text-lg p-1.5 px-4 rounded-3xl hover:text-black hover:bg-white hover:outline' 
+                        onClick={handleClick}>
+					Sign In
+				</button>
+			</div>
+		</div>
+	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import Navbar from "@/Components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -15,7 +16,8 @@ const geistMono = localFont({
 export default function Home() {
   return (
       <main>
-        Hello World
+        <Navbar />
+        Home Page
       </main>
   );
 }
