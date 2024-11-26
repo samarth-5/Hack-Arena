5f4af48552ed23147e31e45e4dd3961713c3ac11
Samarth Narayan <kayaniv321@gmail.com>
2024-11-26 17:11:26 +0530

    added jump game problem

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <>
-		  <title>Hack Arena</title>
+		  <title>Hacker's Arena</title>
 		  <meta name='viewport' content='width=device-width, initial-scale=1' />
 		  <link rel='icon' href='/favicon.ico' />
 		  <meta name='description'
