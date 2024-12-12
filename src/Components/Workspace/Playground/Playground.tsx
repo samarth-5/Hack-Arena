431e65f5471d695518adc4ebb5e30974c56004c2
Samarth Narayan <kayaniv321@gmail.com>
2024-12-12 20:25:48 +0530

    created dynamic testcases

--- src/Components/Workspace/Playground/Playground.tsx
+++ src/Components/Workspace/Playground/Playground.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PreferenceNav from './PreferenceNav'
 import Split from 'react-split'
 import CodeMirror from "@uiw/react-codemirror";
@@ -13,6 +13,7 @@ type Props = {
 
 export default function Playground({problem}: Props) {
   
+  const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
 
   return (
     <div className='flex flex-col bg-white relative overflow-x-hidden'>
@@ -34,39 +35,32 @@ export default function Playground({problem}: Props) {
 						  </div>
 					  </div>
 
-          <div className="flex">
-  {/* Case 1 */}
-            <div className="mr-2 items-start mt-2">
-              <div className="flex flex-wrap items-center gap-y-4">
+          <div className='flex'>
+						{problem.examples.map((example, index) => (
+							<div
+								className='mr-2 items-start mt-2 '
+								key={example.id} onClick={() => setActiveTestCaseId(index)}>
+								<div className='flex flex-wrap items-center gap-y-4'>
                 <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-gradient-to-b from-gray-900 to-black 
                                 hover:from-gray-800 hover:to-black text-white rounded-full px-4 py-1 cursor-pointer whitespace-nowrap shadow-md">
-                  Case 1
-                </div>
-              </div>
-            </div>
-
-  {/* Case 2 */}
-            <div className="mr-2 items-start mt-2">
-              <div className="flex flex-wrap items-center gap-y-4">
-                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-gradient-to-b from-gray-900 to-black 
-                                hover:from-gray-800 hover:to-black text-white rounded-full px-4 py-1 cursor-pointer whitespace-nowrap shadow-md">
-                  Case 2
-                </div>
-              </div>
-            </div>
-          </div>
+										Case {index + 1}
+									</div>
+								</div>
+							</div>
+						))}
+					</div>
 
           <div className="font-semibold my-4">
   {/* Input Section */}
             <p className="text-sm font-medium mt-4 text-black">Input:</p>
             <div className="w-full cursor-text rounded-full border px-3 py-[10px] bg-gradient-to-b from-gray-800 to-black text-white shadow-inner mt-2">
-              nums: [2,7,1,,15] , target: 9
+              {problem.examples[activeTestCaseId].inputText}
             </div>
 
   {/* Output Section */}
               <p className="text-sm font-medium mt-4 text-black">Output:</p>
               <div className="w-full cursor-text rounded-full border px-3 py-[10px] bg-gradient-to-b from-gray-800 to-black text-white shadow-inner mt-2">
-                [0,1]
+                {problem.examples[activeTestCaseId].outputText}
               </div>
             </div>
           </div>
