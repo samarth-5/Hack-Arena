6dbca47dca0dd1068fdc08b70831441e04e8e779
Samarth Narayan <kayaniv321@gmail.com>
2024-12-20 11:55:12 +0530

    created ui of contest preview

---
+++ src/Components/ContestQuestions.tsx
@@ -0,0 +1,112 @@
+import { auth, firestore } from '@/firebase/firebase';
+import { DBProblem } from '@/Utils/types/problem';
+import { collection, doc, getDoc, getDocs, orderBy, query } from 'firebase/firestore';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { BsCheckCircle } from 'react-icons/bs';
+
+type Props = {
+  setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function ContestQuestions({ setLoadingProblems }: Props) {
+
+    const problems = useGetProblems(setLoadingProblems);
+    const solvedProblems = useGetSolvedProblems();
+    console.log(solvedProblems);
+
+  return (
+    <tbody>
+      {problems.map((problem, idx) => {
+        const difficultyColor =
+          problem.difficulty === 'Easy'
+            ? 'text-green-500'
+            : problem.difficulty === 'Medium'
+            ? 'text-yellow-400'
+            : 'text-red-500';
+
+        return (
+          <tr
+            className={`${
+              idx % 2 === 1 ? 'bg-gray-200' : 'bg-white'
+            } hover:bg-gray-300 transition-all duration-300`}
+            key={problem.id}
+          >
+            <td className="px-6 py-3">
+                  {problem.link ? (
+									<Link
+										href={problem.link}
+										className='hover:text-blue-600 cursor-pointer text-[15px] font-semibold'
+										target='_blank'
+									>
+										{problem.title}
+									</Link>
+								) : (
+									<Link
+										className='hover:text-blue-600 cursor-pointer text-[15px] font-semibold'
+										href={`/problems/${problem.id}`}
+									>
+										{problem.title}
+									</Link>
+								)}
+            </td>
+
+            <td className={`px-6 py-3 ${difficultyColor} font-semibold`}>
+              {problem.difficulty}
+            </td>
+            <td className="px-6 py-3 font-semibold">
+              100
+            </td>
+            <th className="px-6 py-3 font-medium">
+              {solvedProblems.includes(problem.id) && <BsCheckCircle className='text-green-500' fontSize={"18"} width='18' />}
+            </th>
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+}
+
+function useGetProblems(setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>) {
+	const [problems, setProblems] = useState<DBProblem[]>([]);
+
+	useEffect(() => {
+		const getProblems = async () => {
+			// fetching data logic
+			setLoadingProblems(true);
+			const q = query(collection(firestore, "problems"), orderBy("order", "asc"));
+			const querySnapshot = await getDocs(q);
+			const tmp: DBProblem[] = [];
+			querySnapshot.forEach((doc) => {
+				tmp.push({ id: doc.id, ...doc.data() } as DBProblem);
+			});
+			setProblems(tmp);
+			setLoadingProblems(false);
+		};
+
+		getProblems();
+	}, [setLoadingProblems]);
+	return problems;
+}
+
+function useGetSolvedProblems() {
+	const [solvedProblems, setSolvedProblems] = useState<string[]>([]);
+	const [user] = useAuthState(auth);
+
+	useEffect(() => {
+		const getSolvedProblems = async () => {
+			const userRef = doc(firestore, "users", user!.uid);
+			const userDoc = await getDoc(userRef);
+
+			if (userDoc.exists()) {
+				setSolvedProblems(userDoc.data().solvedProblems);
+			}
+		};
+
+		if (user) getSolvedProblems();
+		if (!user) setSolvedProblems([]);
+	}, [user]);
+
+	return solvedProblems;
+}
--- src/pages/contests/preview.tsx
+++ src/pages/contests/preview.tsx
@@ -1,5 +1,6 @@
+import ContestQuestions from '@/Components/ContestQuestions';
 import { useRouter } from 'next/router';
-import React from 'react'
+import React, { useState } from 'react'
 
 type Props = {}
 
@@ -7,15 +8,32 @@ export default function preview({}: Props) {
 
   const router = useRouter();
   const { title } = router.query;
+
+  const [loadingProblems, setLoadingProblems] = useState(true);
   
   return (
     <div className="p-6">
       <h1 className="text-3xl font-bold mb-4">Contest Preview</h1>
       {title ? (
-        <p className="text-lg">You are previewing the contest: <strong>{title}</strong></p>
+        <>
+          <p className="text-lg mb-4">Welcome to <strong>{title}</strong></p>
+          <p className="text-lg">You are previewing the contest: <strong>{title}</strong></p>
+        </>
       ) : (
         <p className="text-lg">Loading...</p>
       )}
+
+      <table className="w-full text-sm text-left text-gray-800 sm:w-10/12 max-w-[1200px] mx-auto bg-white shadow-lg rounded-xl mt-6">
+        <thead className="text-[16px] text-black-900 uppercase border-b bg-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3 font-semibold text-left">Question</th>
+            <th scope="col" className="px-6 py-3 font-semibold text-left">Difficulty</th>
+            <th scope="col" className="px-6 py-3 font-semibold text-left">Max. Points</th>
+            <th scope="col" className="px-6 py-3 font-semibold text-left">Status</th>
+          </tr>
+        </thead>
+        <ContestQuestions setLoadingProblems={setLoadingProblems} />
+      </table>
     </div>
   )
 }
